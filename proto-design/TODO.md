dc55b761f46a070bcb77a54d42ddf83ab4634a79
stefanzan <stefanzan@163.com>
2021-09-29 18:23:16 +0800

    [add] package test

--- proto-design/TODO.md
+++ proto-design/TODO.md
@@ -2,3 +2,8 @@
 
 1. expression
 2. string
+3. 特殊字符，比如 "hello world!"中的感叹号
+4. 关键字与名字重复的情况怎么处理
+   import java dot lang dot star
+   4.1 star 与 \*
+   4.2 dot 与 .
---
+++ proto-design/examples/PackageTest.java
@@ -0,0 +1,9 @@
+package org.elasticsearch.search;
+
+import static java.lang.System.*;
+
+public class PackageTest {
+	public static void main(String[] args) {
+		out.println("Hello World!");
+	}
+}
