efdc3f1e6b8cd396c5844dc294c7ecdb9e360753
stefanzan <stefanzan@163.com>
2021-10-13 16:28:04 +0800

    [add] let stmt ast generation of : let _ [dot _]? equal [expression]?

---
+++ voice2code/src/test/java/cn/edu/lyun/kexin/test/text2ast/LetStmtAST6Test.java
@@ -0,0 +1,24 @@
+package cn.edu.lyun.kexin.test.text2ast;
+
+import cn.edu.lyun.kexin.text2ast.*;
+import cn.edu.lyun.kexin.text2pattern.nfa.RegexSet;
+import cn.edu.lyun.kexin.text2pattern.pattern.Pattern;
+import cn.edu.lyun.kexin.text2pattern.pattern.PatternSet;
+import com.github.javaparser.ast.*;
+
+public class LetStmtAST6Test {
+
+	public static void main(String[] args) {
+		// let _ [dot _]? equal [expression]?
+		String[] textList = { "let world equal expression", "let hello dot world equal" };
+
+		for (String text : textList) {
+			Pattern pattern = RegexSet.compile(new PatternSet()).matchPattern(text);
+			System.out.println(pattern.showInstance());
+
+			AST stmt = new LetStmtAST6();
+			Node node = stmt.generate(pattern);
+			System.out.println(node);
+		}
+	}
+}
