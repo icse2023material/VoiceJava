c0301b938370c5c9926366be290f14cb51cf507e
stefanzan <stefanzan@163.com>
2021-10-13 11:49:27 +0800

    [add] try-catch ast generation

---
+++ voice2code/src/main/java/cn/edu/lyun/kexin/text2ast/TryAST.java
@@ -0,0 +1,13 @@
+package cn.edu.lyun.kexin.text2ast;
+
+import com.github.javaparser.ast.*;
+import com.github.javaparser.ast.stmt.TryStmt;
+
+import cn.edu.lyun.kexin.text2pattern.pattern.Pattern;
+
+public class TryAST {
+	public Node generate(Pattern pattern) {
+		return new TryStmt();
+	}
+
+}
