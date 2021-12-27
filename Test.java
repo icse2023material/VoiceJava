2f5ae86953824146afc3bcbb154e21946771f452
stefanzan <stefanzan@163.com>
2021-12-27 22:13:39 +0800

    [refactor] Display of test result

--- Test.java
+++ Test.java
@@ -1,8 +1,7 @@
 public class HelloWorld {
 
-    public int sayHello(Name[] names) {
-        if (counter < 10) {
-            return 100;
-        }
+    public void sayHello(Name[] names) {
+        if (counter < 10)
+            return;
     }
 }
--- voice2code/src/test/java/cn/edu/lyun/kexin/test/text2code/RunMultipleTest.java
+++ voice2code/src/test/java/cn/edu/lyun/kexin/test/text2code/RunMultipleTest.java
@@ -2,6 +2,7 @@ package cn.edu.lyun.kexin.test.text2code;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class RunMultipleTest {
 	public static final String ANSI_RED = "\u001B[31m";
@@ -13,6 +14,7 @@ public class RunMultipleTest {
 		File directoryPath = new File(dir + "/voice2code/src/test/java/cn/edu/lyun/kexin/test/text2code/testcases");
 		File filesList[] = directoryPath.listFiles();
 		int passCount = 0, failCount = 0;
+		ArrayList<String> failedFileList = new ArrayList<String>();
 		for (File file : filesList) {
 			if (!RunMultipleTest.getFileExtension(file).equals(".voiceJava")) {
 				continue;
@@ -27,12 +29,13 @@ public class RunMultipleTest {
 				passCount++;
 			} else {
 				failCount++;
+				failedFileList.add(file.getName());
 			}
 			System.out.println(
 					(isEqual ? ANSI_GREEN + "true" + ANSI_RESET : ANSI_RED + "false" + ANSI_RESET));
 		}
 
-		RunMultipleTest.output(passCount, failCount);
+		RunMultipleTest.output(passCount, failCount, failedFileList);
 	}
 
 	private static String getFileExtension(File file) {
@@ -44,13 +47,19 @@ public class RunMultipleTest {
 		return name.substring(lastIndexOf);
 	}
 
-	private static void output(int passCount, int failCount) {
+	private static void output(int passCount, int failCount, ArrayList<String> failedArrayList) {
 		System.out.println();
 		System.out.println("-------------------------------------");
 		System.out.println("Test Result:");
 		System.out.println("Total: " + (passCount + failCount));
 		System.out.println("Pass : " + ANSI_GREEN + passCount + ANSI_RESET);
-		System.out.println("Fail : " + ANSI_RED + failCount + ANSI_RED);
+		System.out.println("Fail : " + ANSI_RED + failCount + ANSI_RED + ANSI_RESET);
+		if (failCount != 0) {
+			System.out.println("Failed file name list:");
+			for (String str : failedArrayList) {
+				System.out.println("     " + str);
+			}
+		}
 	}
 
 }
