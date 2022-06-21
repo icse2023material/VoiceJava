aeca0230bc8ac6b01e15ecb15793075a6a411f48
stefanzan <stefanzan@163.com>
2022-06-21 14:42:46 +0800

    [refactor] all testcases passed

--- Test.java
+++ Test.java
@@ -1,15 +1,7 @@
 public class HelloWorld {
 
     public void sayHello(Name[] names) {
-        for (int i = 0; i < names.length; i++) {
-            if (i > 90) {
-                continue;
-            } else if (i == 90) {
-                continue;
-            } else {
-                notify();
-                continue;
-            }
-        }
+        if (counter < 10)
+            return;
     }
 }
